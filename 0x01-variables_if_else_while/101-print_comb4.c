9b7b9d1647091358a78c24bba9aab079b1124cc3
Ghada-gh <ghadaghmougui@yahoo.com>
2023-09-11 12:31:17 -0700

    new commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,7 +13,7 @@ int main(void)
       for (j = i + 1; j < 10; j++)
         {
 	  for (k = j + 1; k < 10; k++)
-            {
+	    {
 	      putchar(i + '0');
 	      putchar(j + '0');
 	      putchar(k + '0');
@@ -21,9 +21,9 @@ int main(void)
                 {
 		  putchar(',');
 		  putchar(' ');
-                }
-            }
-        }
+		}
+	    }
+	}
     }
   putchar('\n');
   return (0);
