14987a57f465a06b1cc58c4aba1380d5cd7ac1f0
Ghada-gh <ghadaghmougui@yahoo.com>
2023-09-12 11:29:44 -0700

    new commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,18 +3,7 @@
 #include "main.h"
 
 void print_alphabet(void);
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-  return (write(1, &c, 1));
-}
-
+int _putchar(char c);
 /**
  * print_alphabet - prints the alphabet in lowercase*
  * Return: void
