af5a6ffe4870a3ac3a94451eac86ad2565f2fd02
Ghada-gh <ghadaghmougui@yahoo.com>
2023-09-18 15:24:48 -0700

    new commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,15 +9,17 @@ void print_array(int *a, int n);
  * @n: The number of elements of the array to be printed.
  * Return: void.
  */
+
 void print_array(int *a, int n)
 {
   int i;
 
   for (i = 0; i < n; i++)
     {
-      printf("%d", a[i]);
-      if (i != n - 1)
-        printf(", ");
+      if (i == n - 1)
+	printf("%d", a[i]);
+      else
+	printf("%d, ", a[i]);
     }
   printf("\n");
 }
