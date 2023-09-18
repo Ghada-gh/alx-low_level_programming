6601f24380222892d12e929d86094738ff968f43
Ghada-gh <ghadaghmougui@yahoo.com>
2023-09-18 16:36:20 -0700

    new commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,11 +8,13 @@ int _atoi(char *s);
  * @s: The string to be converted.
  * Return: The integer value of the string.
  */
+
 int _atoi(char *s)
 {
   int i = 0;
   int sign = 1;
   int num = 0;
+  int is_num = 0;
 
   while (s[i] == ' ')
     i++;
@@ -23,11 +25,16 @@ int _atoi(char *s)
 	sign = -1;
       i++;
     }
+
   while (s[i] >= '0' && s[i] <= '9')
     {
+      is_num = 1;
       num = num * 10 + (s[i] - '0');
       i++;
     }
 
-  return num * sign;
+  if (is_num)
+    return num * sign;
+  else
+    return 0;
 }
---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * @brief Generates a random valid password for the program 101-crackme.
+ *
+ * @return A randomly generated valid password.
+ */
+char *generate_password(void)
+{
+  char *password;
+  int i;
+  password = malloc(sizeof(char) * 8);
+  if (password == NULL)
+    return (NULL);
+  for (i = 0; i < 8; i++)
+    {
+      password[i] = (rand() % 26) + 'a';
+    }
+  return (password);
+}
+int main(void)
+{
+  char *password;
+  srand(time(NULL));
+  password = generate_password();
+  printf("./101-crackme \"%s\"\n", password);
+  free(password);
+  return (0);
+}
