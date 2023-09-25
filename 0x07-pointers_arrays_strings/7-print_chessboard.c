12f4b1f4a65a01c7d39be9e39564f89283213537
Ghada-gh <ghadaghmougui@yahoo.com>
2023-09-25 11:39:32 -0700

    new commit

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_chessboard(char (*a)[8]);
+
+/**
+ * print_chessboard - prints a chessboard
+ * @a: the chessboard to print
+ * Return: chessboeard. 
+ */
+
+void print_chessboard(char (*a)[8])
+{
+  int i, j;
+
+  for (i = 0; i < 8; i++)
+    {
+      for (j = 0; j < 8; j++)
+	{
+	  printf("%c ", a[i][j]);
+	}
+      printf("\n");
+    }
+}
