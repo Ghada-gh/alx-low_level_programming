5a0ce25c0aa19f32f29547720e353fd7a7a7b5be
Ghada-gh <ghadaghmougui@yahoo.com>
2023-10-10 10:32:09 -0700

    new commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+
+
+/**
+ * main - function that prints the name of the file it was compiled from.
+ *
+ * Return: 0.
+ */
+
 int main()
 {
   printf("%s\n", __FILE__);
-  return 0;
+  return (0);
 }
